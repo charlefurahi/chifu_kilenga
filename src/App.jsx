e4c05b3ec188081292e0a974610b6340a852c4cf
charlefurahi <charlefurahi@gmail.com>
2026-01-31 16:51:10 +0300

    Refactor App component to use self-closing tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import NavigationBar from "./components/NavigationBar"
-import HeroSection from "./components/HeroSection"
-import AboutSection from "./components/AboutSection"
-import Footer from "./components/Footer"
-function App(){
-  return(
+import NavigationBar from "./components/NavigationBar";
+import HeroSection from "./components/HeroSection";
+import AboutSection from "./components/AboutSection";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
     <>
-    <NavigationBar></NavigationBar>
-    <HeroSection></HeroSection>
-    <AboutSection></AboutSection>
-    <Footer></Footer>
+      <NavigationBar />
+      <HeroSection />
+      <AboutSection />
+      <Footer />
     </>
-  )
+  );
 }
-export default App
+
+export default App;
